bec4c0f363cbea753dbd8bf4f71c903a631280a3
Joaquín Núñez <contacto@joaquinnunez.cl>
2011-12-03 14:09:52 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Junar API Python Client #
+Unofficial API Python Client implementation for the Junar.com API, a service to collect, organize, use and share data.
+
+## Usage ##
+
+    datastream = DataStream('TEPCO-STOCK-QUOTE')
+    print datastream.invoke(output = 'json_array')
+
+## License ##
+(Released under MIT License since v0.0.1)
+
+Copyright (c) 2011 Joaquín Núñez <josejnv[at]gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ junar_api.py
@@ -0,0 +1,64 @@
+import urllib
+import json
+import decimal
+
+class DataStream:
+
+    def __init__(self, guid):
+        self.auth_key = 'YOUR_AUTH_KEY'
+        self.guid = guid
+        self.base_uri = 'http://apisandbox.junar.com'
+        self.response = None
+
+        """
+        for default junar json leave output blank, its structure is explained here http://wiki.junar.com/index.php/API#JSON_Structure
+        other options are: 
+        - prettyjson
+        - json_array, basic javascript array of arrays
+        - csv
+        - tsv
+        - excel
+        """
+        self.output = '';
+
+
+    def get_guid(self):
+        return self.guid
+
+    def invoke(self, params = [], output = ''):
+
+        # create the URL
+        query = {'auth_key': self.auth_key}
+        for index in params:
+            query['pArgument%d' % index] = params[index];
+
+        if output != '':
+            self.output = output
+            query['output'] = output
+
+        url = '/datastreams/invoke/%s?%s' % (self.guid, urllib.urlencode(query))
+        return self.call_uri(url);
+
+    def info(self):
+        # create the URL
+        query = {'auth_key': self.auth_key}
+        url = '/datastreams/invoke/%s?%s' % (self.guid, urllib.urlencode(query))
+        return self.call_uri(url);
+
+    def call_uri(self, url):
+        # get the url
+        # you could also use cURL here, it has better performance but i dont
+        # know if you have cURL installed
+        network_object = urllib.urlopen(self.base_uri + url)
+        response       = network_object.read()
+
+        # parsing the content
+        if self.output in ['', 'prettyjson', 'json_array']:
+            self.response = json.loads(response, parse_float=decimal.Decimal)
+
+        return self.response
+
+
+if __name__ == '__main__':
+    datastream = DataStream('TEPCO-STOCK-QUOTE')
+    print datastream.invoke(output = 'json_array')
