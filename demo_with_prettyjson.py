fbe2f8501018cfe6c1ab9e6ef8a4151b26168e30
Joaquín Núñez <contacto@joaquinnunez.cl>
2012-01-26 10:06:24 -0300

    old name demo

--- demo_with_prettyjson.py
+++
@@ -1,6 +0,0 @@
-import junar_api
-junar_api_client = junar_api.Junar('yourauthkey')
-datastream = junar_api_client.datastream('CURRE-AGAIN-USD-FULL-LIST')
-response = datastream.invoke(output = 'prettyjson')
-for row in response['result']:
-    print row
